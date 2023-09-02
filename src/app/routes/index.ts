dd1f688d8808b04678e7d42aa20cdca7a679d1d7
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2023-09-02 21:46:09 +0600

    Auth Added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { UserRoutes } from '../modules/user/user.routes';
+import { AuthRouter } from '../modules/auth/auth.routes';
 
 const router = express.Router();
 
@@ -8,7 +9,11 @@ const moduleRoutes = [
   {
     path: "/users",
     routes: UserRoutes
-  }
+  },
+  {
+    path: "/auth",
+    routes: AuthRouter
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.routes));
