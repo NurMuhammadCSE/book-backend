d600cdcd62bcc6dc37d1690df460d984a432afd9
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2023-09-02 22:37:23 +0600

    Routes Addedd

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,20 +1,45 @@
 import express from 'express';
-import { UserRoutes } from '../modules/user/user.routes';
 import { AuthRouter } from '../modules/auth/auth.routes';
+import { BookRouter } from '../modules/book/book.routes';
+import { CategoryRoutes } from '../modules/category/category.routes';
+import { OrderRoutes } from '../modules/order/order.routes';
+import { ProfileRoutes } from '../modules/profile/profile.routes';
+import { ReviewAndRating } from '../modules/reviewAndRating/reviewAndRating.routes';
+import { UserRoutes } from '../modules/user/user.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "/users",
-    routes: UserRoutes
+    path: '/auth',
+    route: AuthRouter,
   },
   {
-    path: "/auth",
-    routes: AuthRouter
+    path: '/profile',
+    route: ProfileRoutes,
+  },
+  {
+    path: '/user',
+    route: UserRoutes,
+  },
+  {
+    path: '/categories',
+    route: CategoryRoutes,
+  },
+  {
+    path: '/books',
+    route: BookRouter,
+  },
+  {
+    path: '/reviewandrating',
+    route: ReviewAndRating,
+  },
+  {
+    path: '/orders',
+    route: OrderRoutes,
   },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.routes));
+moduleRoutes.forEach(route => router.use(route.path, route.route));
 export default router;
